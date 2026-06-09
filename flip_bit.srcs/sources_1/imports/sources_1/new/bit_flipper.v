f21750da77e6fad9b03eb41b885e36c8d5c55e20
Kato <kato.mizuki.ksu@gmail.com>
2026-06-09 18:13:38 +0900

    Create bit_flipper.v

---
+++ flip_bit.srcs/sources_1/imports/sources_1/new/bit_flipper.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/01/02 14:45:14
+// Design Name: 
+// Module Name: bit_flipper
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bit_flipper (
+    input wire clk,          // システムクロック (125MHz)
+    input wire rst_n,        // リセット信号 (Active Low)
+    input wire dme_in,       // 差動マンチェスタ入力信号 (12.5MHz)
+);
+
+endmodule
